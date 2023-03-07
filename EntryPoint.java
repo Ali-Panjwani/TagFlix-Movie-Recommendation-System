869f150928b15651f322d92763cbc893e0250cff
Ali-Panjwani <panjwani.ali2001@gmail.com>
2023-03-07 20:10:02 +0500

    Code to check LinkReader

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EntryPoint {
     public static void main(String[] args) {
+        LinksReader linksReader = new LinksReader();
+        linksReader.CsvReader("data\\links.csv");
+        HashMap<String, Links> link = linksReader.getLinksMap();
 
+        if (link.isEmpty()) {
+            System.out.println("The HashMap is empty.");
+        } else {
+            for(Map.Entry<String, Links> link1 : link.entrySet()){
+                String key = link1.getKey();
+                Links value = link1.getValue();
+
+                System.out.println(key+": "+value.getImdbId()+"(IMDB) : "+value.getTmdbId()+"(TMDB)");
+            }
+        }
     }
 }
