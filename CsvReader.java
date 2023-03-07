56721a55a8b3be918932ee20f847c7ef9832c37b
Ali-Panjwani <panjwani.ali2001@gmail.com>
2023-03-07 11:31:15 +0500

    Overridden CsvReader in class LinksReader and RatingsReader

--- CsvReader.java
+++ CsvReader.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,7 +13,7 @@ public interface CsvReader {
 
 class MoviesReader implements CsvReader{
 
-    HashMap<String,Movies> movieMaps = new HashMap<>();
+    HashMap<String,Movies> MovieMaps = new HashMap<>();
 
     @Override
     public void CsvReader(String filepath) {
@@ -21,39 +22,97 @@ class MoviesReader implements CsvReader{
             String line = "";
 
             while((line = br.readLine()) != null ) {
-                Movies movie = new Movies();
+                Movies Movie = new Movies();
                 String[] data = line.split(",");
-                if(movieMaps.containsKey(data[0])) {
+                if(MovieMaps.containsKey(data[0])) {
                     continue;
                 } else {
-                    movie.setMovieId(data[0]);
-                    movie.setTitle(data[1]);
-                    movie.setGenre(data[2].split("|"));
-                    movieMaps.put(data[0], movie);
+                    Movie.setMovieId(data[0]);
+                    Movie.setTitle(data[1]);
+                    Movie.setGenre(data[2].split("|"));
+                    MovieMaps.put(data[0], Movie);
                 }
             }
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public HashMap<String, Movies> getMovieMap() {
-        return movieMaps;
+        return MovieMaps;
     }
 }
 
 class LinksReader implements CsvReader{
+
+    HashMap<String,Links> LinkMaps = new HashMap<>();
     @Override
     public void CsvReader(String filepath) {
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line = "";
 
+            while((line = br.readLine()) != null ) {
+                Links Link = new Links();
+                String[] data = line.split(",");
+                if(LinkMaps.containsKey(data[0])) {
+                    continue;
+                } else {
+                    Link.setMovieId(data[0]);
+                    Link.setImdbId(data[1]);
+                    Link.setTmdbId(data[2]);
+                    LinkMaps.put(data[0], Link);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public HashMap<String, Links> getLinksMap() {
+        return LinkMaps;
     }
 }
 
 class RatingsReader implements CsvReader{
+
+    HashMap<String, Ratings> RatingMap = new HashMap<>();
+
     @Override
     public void CsvReader(String filepath) {
 
+        try(BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line = "";
+
+            while((line = br.readLine()) != null) {
+                Ratings Rating = new Ratings();
+                String[] data = line.split(",");
+                if(RatingMap.containsKey(data[1])){
+                    continue;
+                } else {
+                    Rating.setMovieId(data[1]);
+                    Rating.setRating(Integer.parseInt(data[2]));
+                    RatingMap.put(data[1],Rating);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public HashMap<String, Ratings> getRatingMap(){
+        return RatingMap;
     }
 }
 
