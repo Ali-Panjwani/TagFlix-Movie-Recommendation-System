e4f97f2d665f7a2c437698f325c11e2997df86e0
Ali-Panjwani <panjwani.ali2001@gmail.com>
2023-03-07 11:28:49 +0500

    Completed Links.java with movieId, imdbId, tmdbId and their setters/getters

--- Links.java
+++ Links.java
@@ -4,5 +4,35 @@ import java.util.ArrayList;
 public class Links {
 
     private String movieId;
+    private String imdbId;
+    private String tmdbId;
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTmdbId() {
+        return tmdbId;
+    }
+
+    public void setTmdbId(String tmdbId) {
+        this.tmdbId = tmdbId;
+    }
+
+    public Links() {
+
+    }
 
 }
