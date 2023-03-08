d00c998a8beddb0eec98d184c2695bcefb0f2dd1
Ali-Panjwani <panjwani.ali2001@gmail.com>
2023-03-08 14:02:34 +0500

    Changed some data types and modified the functionality of setRatings

--- Ratings.java
+++ Ratings.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 public class Ratings {
 
     private String movieId;
-    private int rating = 0;
-    private int rating_count;
+    private float rating = 0;
+    private float rating_count;
 
     public String getMovieId() {
         return movieId;
@@ -15,18 +15,29 @@ public class Ratings {
         this.movieId = movieId;
     }
 
-    public int getRating() {
+    public float getRating() {
         return rating;
     }
 
-    public void setRating(int new_rating) {
+    public void setRating(String new_rating_str) {
+        float new_rating = 0;
+        try {
+            if (new_rating_str.contains(".")) {
+                new_rating = Float.parseFloat(new_rating_str);
+            } else {
+                new_rating = (float) Integer.parseInt(new_rating_str);
+            }
+        } catch(NumberFormatException e){
+            System.out.println("Issue: "+new_rating_str);
+        }
         rating_count++;
 
-        int prev_rating = this.rating;
-        this.rating = ((rating * (rating_count-1)) + new_rating) / rating_count;
+        float prev_rating = this.rating;
+        this.rating = (((rating * (rating_count-1)) + new_rating) / rating_count);
     }
 
-    public int getrating_count() {
+
+    public float getrating_count() {
         return rating_count;
     }
 
